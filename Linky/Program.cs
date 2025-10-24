5abb910a71b0785cd60ab944a6f7c2ccae552a40
Andrei Alionte <andrei.alionte@gmail.com>
2025-10-24 17:21:50 +0300

    .

--- Linky/Program.cs
+++ Linky/Program.cs
@@ -113,7 +113,7 @@ namespace Linky
             {
                 options.ListenAnyIP(5000, listenOptions =>
                 {
-                    listenOptions.Protocols = HttpProtocols.Http3;
+                    listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
                 });
             });
 
