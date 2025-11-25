cbdb5290e27ba27c7398780954696b90872e0564
Andrei Alionte <andrei.alionte@gmail.com>
2025-11-25 22:37:10 +0200

    update to .NET 10

--- Linky/Migrations/20251018141026_Init.Designer.cs
+++ Linky/Migrations/20251018141026_Init.Designer.cs
@@ -21,7 +21,7 @@ namespace Linky.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.10")
+                    .HasAnnotation("ProductVersion", "10.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
