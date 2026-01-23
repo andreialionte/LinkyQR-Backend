96cced1d54c2366b729522959b294c7935da8855
Andrei Alionte <andrei.alionte@gmail.com>
2026-01-23 22:37:09 +0200

    .

--- Linky/Repository/ActiveVisitorRepository.cs
+++ Linky/Repository/ActiveVisitorRepository.cs
@@ -61,7 +61,7 @@ namespace Linky.Repository
                 _context.ActiveVisitors.Update(existing);
                 await _context.SaveChangesAsync();
 
-                // update may affect cached list/order → invalidate caches
+                // update may affect cached list/order invalidate caches
                 await _cacheService.RemoveAsync(VISITORS_LIST_KEY);
                 await _cacheService.RemoveAsync(VISITOR_COUNT_KEY);
 
