040281d8fb1e0a3e4339520a190f3df86ec6dd59
Andrei Alionte <andrei.alionte@gmail.com>
2026-09-19 18:45:39 +0300

    .

---
+++ Linky/Service/MqttDI.cs
@@ -0,0 +1,28 @@
+﻿using Linky.Config;
+using Linky.IService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Linky.Service
+{
+    public static class MqttDI
+    {
+        public static IServiceCollection AddMqttServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Bind MQTT options from appsettings section "Mqtt"
+            services.Configure<MqttConfig>(configuration.GetSection("Mqtt"));
+
+            // Register MqttClientService as Singleton and HostedService
+            services.AddSingleton<MqttClientService>();
+            services.AddSingleton<IMqttClientService>(sp => sp.GetRequiredService<MqttClientService>());
+            services.AddHostedService(sp => sp.GetRequiredService<MqttClientService>());
+
+            // Register CacheBroadcastService & EmqxAuthService
+            services.AddSingleton<ICacheBroadcastService, CacheBroadcastService>();
+            services.AddSingleton<IEmqxAuthService, EmqxAuthService>();
+
+            return services;
+        }
+    }
+}
+
